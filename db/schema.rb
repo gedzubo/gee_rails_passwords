40fea8aad6815ff5ed64644f749e22e082de9cea
Gediminas Z <gediminas.zubovicius@gmail.com>
2024-11-23 15:36:15 +0000

    Updating rails version to 8 (from 7)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_08_164108) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_23_153441) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name"
